422c774b13fff43437eaca6d895e5a7d116a42b6
Tati-Zhurr <zhtv88@gmail.com>
2022-10-15 22:13:28 +0200

    feat: list

--- src/remove-from-list.js
+++ src/remove-from-list.js
@@ -22,11 +22,69 @@ const { NotImplementedError, ListNode } = require('../extensions/index.js');
  *   }
  * }
  */
-function removeKFromList(/*l, k*/) {
-  throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+ 
+function removeKFromList(l, k) {
+  if (l ===null){
+    return l;
+  }
+  while (l.value === k){
+    l=l.next;
+  }
+  thisNode =l;
+  nextNode = thisNode.next;
+  nextNextNode= nextNode.next;
+  while(nextNode !==null){
+    if (nextNode.value ===k){
+      if(nextNextNode){
+        if(nextNextNode.value ===k){
+         thisNode.next = nextNextNode.next;
+        }
+        
+      } else{
+        thisNode.next = nextNode.next;
 
-}
+      }
+      if (thisNode.next === null){
+        break;
+      }
+    }
+   thisNode = thisNode.next;
+   nextNode = thisNode.next;
+  }
+    return l;
+  
+  }
+ /* let resultList = l;
+  while ((resultList)&&(resultList.value ===k)){
+    resultList = resultList.next;
+  };
+  let current = resultList.next;
+  while ((current)&&(current.value ===k)){
+    current = current.next;
+  };
+  while (current.next){
+    if (current.next.value !==k){
+      current = current.next;
+    }else{
+      current.next =current.next.next;
+    }*/
+  
+ /* let linkResult = new LinkedList;
+  if (l.value !==k){
+    let node = new NodeList(l.value);
+    if (linkResult.head === null){
+      linkResult.head = node;
+    }
+    let current = linkResult.head.next;
+    while(current){
+      current = current.next;
+    }
+    current = new NodeList(l.value);
+  }
+ // throw new NotImplementedError('Not implemented');
+    // remove line with error and write your code here
+*/
+
 
 module.exports = {
   removeKFromList
